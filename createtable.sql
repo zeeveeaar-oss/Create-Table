2ea1c3b9d7ad6f5da5da1e4811a4cd4da677eda8
zeeveeaar-oss <zeeveeaar@gmail.com>
2026-08-29 11:28:21 +0530

    Add files via upload

---
+++ createtable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE supplier (
+    SNO TEXT PRIMARY KEY,
+    SNAME TEXT,
+    STATUS INTEGER,
+    CITY TEXT
+);
+
+INSERT INTO supplier (SNO, SNAME, STATUS, CITY) VALUES
+('S1', 'Smith', 20, 'London'),
+('S2', 'Jones', 10, 'Paris'),
+('S3', 'Blake', 30, 'Paris'),
+('S4', 'Clarke', 20, 'London'),
+('S5', 'Adams', 30, 'Athens');
+
+SELECT * FROM supplier;
